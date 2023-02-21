f9edeedfd51cb7a488f3a49cc30b808972c8875d
MukeshSmart <mukesh14mca60@gmail.com>
2023-02-21 11:44:57 +0530

    Scoped POC

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddMvc();
-builder.Services.AddSingleton<IStudentRepository, StudentRepository>();
+builder.Services.AddScoped<IStudentRepository, StudentRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
